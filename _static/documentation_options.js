7fe7ccc73918a23baad51732b497cde7c338a5d2
ptth222 <ptth222@users.noreply.github.com>
2024-04-01 05:11:22 +0000

    Update documentation.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gfa4e18d',
+    VERSION: '0.1.dev1+g144a391',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
